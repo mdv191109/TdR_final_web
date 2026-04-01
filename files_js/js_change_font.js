9e0c0cdb747680b6017b58ef34043155b8db189e
mdv191109 <mdv191109@gmail.com>
2026-04-01 18:29:02 +0200

    commit: it it over yet?

--- files_js/js_change_font.js
+++ files_js/js_change_font.js
@@ -2,11 +2,11 @@
       const style = document.getElementById("font-style");
       const button = document.getElementById("font_button");
 
-      if (style.getAttribute("href") === "/files_css/typography_normal.css") {
-        style.setAttribute("href", "/files_css/typography_dyslexic.css");
+      if (style.getAttribute("href") === "files_css/typography_normal.css") {
+        style.setAttribute("href", "files_css/typography_dyslexic.css");
         button.textContent = "normal mode";
       } else {
-        style.setAttribute("href", "/files_css/typography_normal.css");
+        style.setAttribute("href", "files_css/typography_normal.css");
         button.textContent = "dyslexic mode";
       }
     }
--- files_js/js_change_palette.js
+++ files_js/js_change_palette.js
@@ -2,21 +2,21 @@ function change_palette() {
     const style = document.getElementById("palette-style");
     const button_mode = document.getElementById("palette_button");
 
-    if (style.getAttribute("href") === "/files_css/palette_lightmode.css") {
-        style.setAttribute("href", "/files_css/palette_darkmode.css");
+    if (style.getAttribute("href") === "files_css/palette_lightmode.css") {
+        style.setAttribute("href", "files_css/palette_darkmode.css");
         button_mode.textContent = "modo claro";
 
-    } else if (style.getAttribute("href") === "/files_css/palette_darkmode.css") {
-        style.setAttribute("href", "/files_css/palette_lightmode.css");
+    } else if (style.getAttribute("href") === "files_css/palette_darkmode.css") {
+        style.setAttribute("href", "files_css/palette_lightmode.css");
         button_mode.textContent = "modo oscuro";
     }
 
-    else if (style.getAttribute("href") === "/files_css/palette_contrast_light.css") {
-        style.setAttribute("href", "/files_css/palette_contrast_dark.css");
+    else if (style.getAttribute("href") === "files_css/palette_contrast_light.css") {
+        style.setAttribute("href", "files_css/palette_contrast_dark.css");
         button_mode.textContent = "modo oscuro";
     }
     else {
-        style.setAttribute("href", "/files_css/palette_contrast_light.css");
+        style.setAttribute("href", "files_css/palette_contrast_light.css");
         button_mode.textContent = "modo claro";
     }
 }
